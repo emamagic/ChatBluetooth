547872d69e81f24f8fef4939746854f4ad7db4fc
developer developer <developermagic7@gmail.com>
2021-04-04 05:21:21 -0700

    kotlin_dsl_added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 buildscript {
-    ext.kotlin_version = "1.4.32"
     repositories {
         google()
         jcenter()
+        mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.1.3"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath (Build.build_tools)
+        classpath (Build.kotlin_gradle_plugin)
+        classpath (Build.navigation_safe_args)
+        classpath (Build.hilt)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -18,9 +21,10 @@ allprojects {
     repositories {
         google()
         jcenter()
+        mavenCentral()
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean").configure{
+    delete("build")
 }
